11c28a4a1ce758fef0c5de653d0001b809a27a82
Joaquin Gonzalez <jpgonzalezra@gmail.com>
2019-01-24 14:24:31 -0300

    [UPDATE] rename MarmoFactory class to MarmoStork

--- test/TestMarmoWallets.js
+++ test/TestMarmoWallets.js
@@ -1,5 +1,5 @@
 const Marmo = artifacts.require('./Marmo.sol');
-const MarmoCreator = artifacts.require('./MarmoFactory.sol');
+const MarmoStork = artifacts.require('./MarmoStork.sol');
 const DepsUtils = artifacts.require('./DepsUtils.sol');
 const TestERC20 = artifacts.require('./TestERC20.sol');
 const TestOutOfGasContract = artifacts.require('./TestOutOfGasContract.sol');
@@ -54,13 +54,13 @@ contract('Marmo wallets', function (accounts) {
         }
 
         // Setup contracts
-        creator = await MarmoCreator.new();
+        creator = await MarmoStork.new();
         depsUtils = await DepsUtils.new();
         testToken = await TestERC20.new();
     });
     describe('Create marmo wallets', function () {
         it('Should reveal the marmo wallet', async function () {
-            const creator = await MarmoCreator.new();
+            const creator = await MarmoStork.new();
             await creator.reveal(accounts[0]);
         });
         it('Should predict the Marmo wallet', async function () {
