a82aee7a2b424427477c48b5c54f2cb7f41081e3
Agustin <agusxrun@gmail.com>
2019-03-09 15:40:49 +0100

    Replace assert by revert relay to INVALID_ADDRESS

--- contracts/Marmo.sol
+++ contracts/Marmo.sol
@@ -100,7 +100,7 @@ contract Marmo {
 
         // The signer 'INVALID_ADDRESS' is considered invalid and it will always throw
         // this is meant to destroy the wallet safely
-        assert(_signer != INVALID_ADDRESS);
+        require(_signer != INVALID_ADDRESS);
 
         // Validate is the msg.sender is the signer or if the provided signature is valid
         require(_signer == msg.sender || _signer == SigUtils.ecrecover2(id, _signature), "Invalid signature");
