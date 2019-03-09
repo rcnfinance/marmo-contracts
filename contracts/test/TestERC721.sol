646ad6e4d9d39b97a322a24a843a04d6f4f33d67
Agustin <agusxrun@gmail.com>
2019-03-09 14:41:57 +0100

    Add receive ERC721 test case

---
+++ contracts/test/TestERC721.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.5.0;
+
+import "openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol";
+
+
+contract TestERC721 is ERC721Full {
+    constructor() public ERC721Full("Test ERC721", "T721") { }
+
+    function mint(address _to, uint256 _id) external {
+        _mint(_to, _id);
+    }
+}
