37b3afcb7bde048474faf697c83e5c88b6b12257
Agustin <agusxrun@gmail.com>
2019-03-09 15:08:05 +0100

    Disable eslint

--- truffle-config.js
+++ truffle-config.js
@@ -3,6 +3,7 @@ module.exports = {
         development: {
             host: '127.0.0.1',
             port: 8545,
+            // eslint-disable-next-line camelcase
             network_id: '*',
         },
     },
